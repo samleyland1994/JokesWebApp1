988c47d16699645e25b9685544e342171b10c454
samleyland1994 <samleyland1994@gmail.com>
2022-11-18 14:27:51 +0000

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokesWebApp1.Models;
+
+namespace JokesWebApp1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesWebApp1.Models.Jokes> Jokes { get; set; }
+    }
+}
---
+++ Models/Jokes.cs
@@ -0,0 +1,15 @@
+﻿namespace JokesWebApp1.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Jokes()
+        {
+
+        }
+
+    }
+}
